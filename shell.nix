15fb24e1a0decd7c5317b438214382c5144d5588
loicsikidi <loic.sikidi@gmail.com>
2025-12-02 00:53:48 +0100

    test attestation

--- shell.nix
+++ shell.nix
@@ -1,17 +1,20 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ee09932cedcef15aaf476f9343d1dea2cb77e261.tar.gz") {} }:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    go
-    goreleaser
-    cosign
-    syft
-  ];
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/ee09932cedcef15aaf476f9343d1dea2cb77e261.tar.gz")
+  { }, }:
+let
+  helpers = import (builtins.fetchTarball
+    "https://github.com/loicsikidi/nix-shell-toolbox/tarball/main") {
+      inherit pkgs;
+    };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ goreleaser cosign syft ] ++ helpers.packages;
 
   shellHook = ''
+    ${helpers.shellHook}
     echo "Development environment ready!"
     echo "  - Go version: $(go version)"
     echo "  - GoReleaser version: $(goreleaser --version | head -n 1)"
     echo "  - Cosign version: $(cosign version)"
+    echo "  - Zizmor version: $(zizmor --version)"
   '';
 }
